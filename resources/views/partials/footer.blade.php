2cb17d53fae0a4c9716266c0405ef1298591d343
RobertoZefi <zefi.roberto2001@gmail.com>
2023-04-05 17:08:29 +0200

    FEAT add footer(storto)

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,159 @@
+<footer>
+        <div class="gadget">
+            <div class="container">
+                <ul class="list">
+                    <li>
+                        <img src="/buy-comics-digital-comics.png" alt="">
+                        <p>digital comics</p>
+                    </li>
+                    <li>
+                        <img src="/buy-comics-merchandise.png" alt="">
+                        <p>dc merchandise</p>
+                    </li>
+                    <li>
+                        <img src="/buy-comics-subscriptions.png" alt="">
+                        <p>subscription</p>
+                    </li>
+                    <li>
+                        <img src="/buy-comics-shop-locator.png" alt="">
+                        <p>comic shop locator</p>
+                    </li>
+                    <li>
+                        <img src="/buy-dc-power-visa.svg" alt="">
+                        <p>dc power visa</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="info">
+            <div class="container">
+                <img class="logo" src="/dc-logo-bg.png" alt="">
+                <div class="row">
+                    <div class="col">
+                        <ul class="info__list">
+                            <li>
+                                <h3>dc comics</h3>
+                            </li>
+                            <li>
+                                <p>Characters</p>
+                            </li>
+                            <li>
+                                <p>Comics</p>
+                            </li>
+                            <li>
+                                <p>Movies</p>
+                            </li>
+                            <li>
+                                <p>TV</p> 
+                            </li>
+                            <li>
+                                <p>Games</p> 
+                            </li>
+                            <li>
+                                <p>Videos</p>
+                            </li>
+                            <li>
+                                <p>News</p>
+                            </li>
+                        </ul>
+
+                        <ul class="info__list">
+                            <li class="title">
+                                <h3>shop</h3>
+                            </li>
+                            <li>
+                                <p>Shop DC</p>
+                            </li>
+                            <li>
+                                <p>Shop DC Collectibles</p>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="col">
+                        <ul class="info__list">
+                            <li>
+                                <h3>dc</h3>
+                            </li>
+                            <li>
+                                <p>Terms Of Use</p>
+                            </li>
+                            <li>
+                                <p>Privacy policy (news)</p>
+                            </li>
+                            <li>
+                                <p>Ad Choices</p>
+                            </li>
+                            <li>
+                                <p>Advertising</p> 
+                            </li>
+                            <li>
+                                <p>Jobs</p> 
+                            </li>
+                            <li>
+                                <p>Subscriptions</p>
+                            </li>
+                            <li>
+                                <p>Tealent Workshops</p>
+                            </li>
+                            <li>
+                                <p>CPSC Certificates</p>
+                            </li>
+                            <li>
+                                <p>Ratings</p>
+                            </li>
+                            <li>
+                                <p>Shop Help</p>
+                            </li>
+                            <li>
+                                <p>Contact Us</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <ul class="info__list">
+                            <li>
+                                <h3>sites</h3>
+                            </li>
+                            <li>
+                                <p>DC</p>
+                            </li>
+                            <li>
+                                <p>MAD Magazine</p>
+                            </li>
+                            <li>
+                                <p>DC Kids</p>
+                            </li>
+                            <li>
+                                <p>DC Universe</p> 
+                            </li>
+                            <li>
+                                <p>DC Power Visa</p> 
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="social">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <p class="button">sign-up now!</p>
+                    </div>
+                    <div class="col__icon">
+                        <h3>follow us</h3>
+                        <div class="icon">
+                            <img src="/footer-facebook.png" alt="">
+                            <img src="/footer-twitter.png" alt="">
+                            <img src="/footer-youtube.png" alt="">
+                            <img src="/footer-pinterest.png" alt="">
+                            <img src="/footer-periscope.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
