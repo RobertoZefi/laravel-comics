ad40bead378f11daa0da9e779f3407170db1b23a
RobertoZefi <zefi.roberto2001@gmail.com>
2023-04-05 16:44:41 +0200

    FEAT header added

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,22 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <img src="/dc-logo.png" alt="">
+        </div>
+
+        <div class="col">
+            <ul class="list fw-semibold">
+                <li>characters</li>
+                <li>comics</li>
+                <li>movies</li>
+                <li>tv</li>
+                <li>games</li>
+                <li>collectibles</li>
+                <li>videos</li>
+                <li>fans</li>
+                <li>news</li>
+                <li>shop</li>
+            </ul>
+        </div>
+    </div>
+</div>
